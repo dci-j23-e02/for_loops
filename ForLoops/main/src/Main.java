72fe82c275130904d627532c98a6dc8ce5a1f143
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-24 10:10:07 +0200

    first exercises added

--- ForLoops/main/src/Main.java
+++ ForLoops/main/src/Main.java
@@ -1,6 +1,56 @@
 public class Main {
 
   public static void main(String[] args) {
+   /*
+   int i = 0;
+   String name = "Safwan";
+     */
+
+    /*
+      while( i < 10){
+        System.out.println("Hello , i is " + i);
+        i= i +3;
+
+      }*/
+
+
+    /*
+ if(i < 10 ){
+        System.out.println("Hello , i is " + i);
+        i= i +3;
+      }*/
+
+     // boolean x = 5 < 6;
+
+
+    /**
+     * while loop
+     * do .. while loop
+     * for loop
+     * forEach loop
+     * */
+
+    /**
+     *     for (initialisation; condition ; increment / decrement){
+     *          // code to be executed
+     *     }
+     * */
+    String name = "Safwan";
+    for (int i = 0; i < name.length() ;  i++){
+      System.out.println("The value of i is " + i);
+      System.out.println("The charAt "+ i + " is "+ name.charAt(i));
+    }
+
+
+    /**
+     * Write a small Java program that reads a phrase from the
+     * console and then check where is the position of character 's' inside it.
+     * if there is no 's' , then print , there is no 's'
+     * 's' could be  Upper or lower case.
+     * hint 1: you might use Scanner class
+     * hint 2: use 'for loop' and you might need' if statement'  inside it.
+     * */
+
 
   }
 }
