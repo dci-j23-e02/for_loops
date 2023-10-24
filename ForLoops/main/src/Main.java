0cc3ae0b7d183c28852aa70bcc57be7e6f4754a6
safwan-kher <safwan.kherallah@digitalcareerinstitute.org>
2023-10-24 13:09:20 +0200

    last version today

--- ForLoops/main/src/Main.java
+++ ForLoops/main/src/Main.java
@@ -1,3 +1,6 @@
+import java.util.Locale;
+import java.util.Scanner; // more readable to mention the class you've used
+
 public class Main {
 
   public static void main(String[] args) {
@@ -35,13 +38,13 @@ public class Main {
      *          // code to be executed
      *     }
      * */
-    String name = "Safwan";
+   /* String name = "Safwan";
     for (int i = 0; i < name.length() ;  i++){
       System.out.println("The value of i is " + i);
       System.out.println("The charAt "+ i + " is "+ name.charAt(i));
     }
 
-
+*/
     /**
      * Write a small Java program that reads a phrase from the
      * console and then check where is the position of character 's' inside it.
@@ -49,8 +52,110 @@ public class Main {
      * 's' could be  Upper or lower case.
      * hint 1: you might use Scanner class
      * hint 2: use 'for loop' and you might need' if statement'  inside it.
+     * hint 3: provide only the first occurrence.
+     * */
+/*
+    Scanner input = new Scanner(System.in);
+    System.out.println("Please enter the phrase of your choice: ");
+    String phrase = input.nextLine();
+    phrase = phrase.toLowerCase(); // 's' could be  Upper or lower case.
+    boolean found = false; // provide only the first occurrence, no need to use it if we use break;
+    int positions = 0; // print the positions outside the loop
+*/
+    /**
+     * i < phrase.length() && !found : return false if one of these returns false
+     *
      * */
 
+    /* Mu*eum* : phrase.length() = 7
+    *  i: 0 -> M (  i < phrase.length() : true )
+    *  i: 1 -> u (  i < phrase.length() : true )
+    *  i: 2 -> * (  i < phrase.length() : true )
+    *  i: 3 -> e (  i < phrase.length() : true )
+    *  i: 4 -> u (  i < phrase.length() : true )
+    *  i: 5 -> m (  i < phrase.length() : true )
+    *  i: 6 -> * (  i < phrase.length() : true )
+    *  i: 7 ->   (  i < phrase.length() : false )
+
+     */
+
+    /* Mu*eums : phrase.length() = 7
+     *  i: 0 -> M (  i < phrase.length() : true )
+     *  i: 1 -> u (  i < phrase.length() : true )
+     *  i: 2 -> * (  i < phrase.length() : true )
+     *  i: 3 -> e (  i < phrase.length() : true )
+     *  i: 4 -> u (  i < phrase.length() : true )
+     *  i: 5 -> m (  i < phrase.length() : true )
+     *  i: 6 -> s (  i < phrase.length() : true ) && ( !found = false) = false
+     *  i: 7 ->   (  i < phrase.length() : false )
+
+     */
+
+    /* Museums : phrase.length() = 7
+     *  i: 0 -> M (  i < phrase.length() : true )
+     *  i: 1 -> u (  i < phrase.length() : true )
+     *  i: 2 -> s (  i < phrase.length() : true )  && ( !found = false) = false // Only first occurrence is provided.
+     *
+
+     */
+/*
+    for (int i = 0; i < phrase.length() && !found ; i++){
+      if(phrase.charAt(i) == 's'){
+        found = true;
+        positions = i + 1 ;
+      }
+    }
+
+   if(positions > 0){
+      System.out.println("We could find the character 's' at position " +  positions );
+    }else{
+      System.out.println("We couldn't find the character 's' inside given phrase " );
+    }
+
+*/
+   // int test;
+   // System.out.println(test); : java: variable test might not have been initialized
+
+
+    // Loops control statements ( break; continue; )
+/*
+    for (int i = 0; i < phrase.length() ; i++){
+      if(phrase.charAt(i) == 's'){
+        positions = i + 1 ;
+        System.out.println(i);
+        break;
+      }
+      System.out.println(i);
+    }
+
+    if(positions > 0){
+      System.out.println("We could find the character 's' at position " +  positions );
+    }else{
+      System.out.println("We couldn't find the character 's' inside given phrase " );
+    }
+
+    */
+
+   // continue; example :
+    /** without // continue;
+     * j:0 -> "Sunday" : {j:1  , print "Monday", j:2}
+     * j:2 -> "Tuesday": {print "Tuesday", j:3 }
+     * j:3 -> "Wednesday": {print "Wednesday", j:4 }
+     * j:4 -> "Thursday": {print "Thursday", j:5 }
+     * j:5 -> "Friday": {print "Friday", j:6 }
+     * j:6 -> "Saturday":  {j:7  , print "weekDays[7]" Index 7 out of bounds for length 7 }
+     * We don't want to have errors , therefore continue wouldf be the best solution
+     * */
+    String[]  weekDays = {"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"};
+    int j =0;
+    while( j < weekDays.length) {
+      if(weekDays[j].equals("Saturday") || weekDays[j].equals("Sunday") ){
+        j++;
+        continue;
+      }
+      System.out.println(weekDays[j]);
+      j++;
+    }
 
   }
 }
